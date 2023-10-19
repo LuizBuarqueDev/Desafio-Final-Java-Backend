4dacfd5c4f0d26e835a5bf76898db097548f7c0d
LuizBuarqueDev <luizgabrielbuarque>
2023-10-19 15:01:35 -0300

    Initial commit

---
+++ src/main/java/dio/teste/DesafioFinalJavaBackend/DesafioFinalJavaBackendApplication.java
@@ -0,0 +1,13 @@
+package dio.teste.DesafioFinalJavaBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioFinalJavaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioFinalJavaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio/teste/DesafioFinalJavaBackend/DesafioFinalJavaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.teste.DesafioFinalJavaBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioFinalJavaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
